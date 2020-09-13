ec349098baf64e533b97256e7bbc59c7fbb956ca
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2020-09-13 13:30:26 +0300

    Updates

---
+++ precache-manifest.b89a9a48af44e56c40e42c31ae2e2c61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fee806cafd22cc4e67b548a13509648",
+    "url": "/portfolio-webpage/index.html"
+  },
+  {
+    "revision": "f532f90eb91beb45b990",
+    "url": "/portfolio-webpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98bdf3a3e655bd23032b",
+    "url": "/portfolio-webpage/static/js/2.95916ed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-webpage/static/js/2.95916ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f532f90eb91beb45b990",
+    "url": "/portfolio-webpage/static/js/main.c9967ccb.chunk.js"
+  },
+  {
+    "revision": "02d7b3dd8013de6dc7e3",
+    "url": "/portfolio-webpage/static/js/runtime-main.601f46ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-webpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
