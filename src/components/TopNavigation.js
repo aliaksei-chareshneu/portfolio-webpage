8bc67114e756a3018d47a1b93b0fddb8720ee951
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2020-09-15 13:25:34 +0300

    Make Navbar responsive (collapses behind a button) and fixed to the top

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -3,18 +3,22 @@ import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
 
 const TopNavigation = () => {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar collapseOnSelect fixed="top" expand="md" bg="dark" variant="dark">
             <Navbar.Brand href="#home">Logo goes here</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link href="#cv">CV</Nav.Link>
-                <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
-            {/* <Form inline>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#cv">CV</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav>
+                {/* <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                     <Button variant="outline-info">Search</Button>
                 </Form> */}
+            </Navbar.Collapse>
+
         </Navbar>
     )
 }
