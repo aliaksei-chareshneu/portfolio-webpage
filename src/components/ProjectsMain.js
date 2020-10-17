f650b81de81bae9718c29424bb921667ed5a9f5c
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2020-10-17 13:39:47 +0300

    Rewrote project items as components; Add NAChRDB project item

--- src/components/ProjectsMain.js
+++ src/components/ProjectsMain.js
@@ -1,105 +1,41 @@
 import React from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Card, Image } from 'react-bootstrap'
 
 const LandingMain = () => {
   return (
     <main id="main-content">
-      <div className="container-fluid" style={{ "z-index": "2" }}>
-        <div className="row">
+      <Container style={{ "z-index": "2" }}>
+        <Card as="article" className="card mb-3">
+          <Row noGutters={true}>
+            <Col sm={3}>
+              <Image src={`${process.env.PUBLIC_URL}/nachrdb-logo-low-res.jpg`} className="card-img" />
+            </Col>
+            <Col sm={9}>
+              <Card.Body>
+                <Card.Title>NAChRDB</Card.Title>
+                {/* TODO: shorten text (twice) */}
+                <Card.Text>NAChRDB is a web application and a database of structural-functional information on nicotinic acetylcholine receptors (nAChRs) – proteins that transfer chemical signals across the nervous system, allowing us to think, feel and move. Due to their paramount importance, nAChRs have been studied for more than 50 years. NAChRDB systematizes scientific knowledge on nAChRs, making it possible to find, interactively visualize, and analyze information on specific nAChR regions in a matter of seconds. Besides, NAChRDB delivers computational predictions of potentially important parts of nAChR. NAChRDB can help to interpret the research data, spot contradictory research findings, and guide new discoveries in the field.</Card.Text>
+              </Card.Body>
+            </Col>
+          </Row>
+        </Card>
 
-        </div>
+        <Card as="article" className="card mb-3">
+          <Row noGutters={true}>
+            <Col sm={3}>
+              <Image src={`${process.env.PUBLIC_URL}/nachrdb-logo-low-res.jpg`} className="card-img" />
+            </Col>
+            <Col sm={9}>
+              <Card.Body>
+                <Card.Title>NAChRDB</Card.Title>
+                {/* TODO: shorten text (twice) */}
+                <Card.Text>NAChRDB is a web application and a database of structural-functional information on nicotinic acetylcholine receptors (nAChRs) – proteins that transfer chemical signals across the nervous system, allowing us to think, feel and move. Due to their paramount importance, nAChRs have been studied for more than 50 years. NAChRDB systematizes scientific knowledge on nAChRs, making it possible to find, interactively visualize, and analyze information on specific nAChR regions in a matter of seconds. Besides, NAChRDB delivers computational predictions of potentially important parts of nAChR. NAChRDB can help to interpret the research data, spot contradictory research findings, and guide new discoveries in the field.</Card.Text>
+              </Card.Body>
+            </Col>
+          </Row>
+        </Card>
 
-        <article className="card mb-3">
-          <div className="row no-gutters">
-            <div className="col-sm-3">
-              <img src="..." className="card-img" alt="..." />
-            </div>
-            <div className="col-sm-9">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
-        </article>
-
-        <article className="card mb-3">
-          <div className="row no-gutters">
-            <div className="col-sm-3">
-              <img src="..." className="card-img" alt="..." />
-            </div>
-            <div className="col-sm-9">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
-        </article>
-
-        <article className="card mb-3">
-          <div className="row no-gutters">
-            <div className="col-sm-3">
-              <img src="..." className="card-img" alt="..." />
-            </div>
-            <div className="col-sm-9">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
-        </article>
-
-        <article className="card mb-3">
-          <div className="row no-gutters">
-            <div className="col-sm-3">
-              <img src="..." className="card-img" alt="..." />
-            </div>
-            <div className="col-sm-9">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
-        </article>
-
-        <article className="card mb-3">
-          <div className="row no-gutters">
-            <div className="col-sm-3">
-              <img src="..." className="card-img" alt="..." />
-            </div>
-            <div className="col-sm-9">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
-        </article>
-
-        <article className="card mb-3">
-          <div className="row no-gutters">
-            <div className="col-sm-3">
-              <img src="..." className="card-img" alt="..." />
-            </div>
-            <div className="col-sm-9">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
-        </article>
-
-      </div>
+      </Container>
     </main>
 
 
