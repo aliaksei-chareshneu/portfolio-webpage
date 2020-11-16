e553bef98a8dcd953e6d9a60eed4b2a5f2fd7330
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2020-11-16 12:04:16 +0300

    Set two-col layout (demo/logo on the left, text on the right)

--- src/components/IndividualProjectMain.js
+++ src/components/IndividualProjectMain.js
@@ -12,41 +12,53 @@ const IndividualProjectMain = (props) => {
             <main id="main-content">
                 <Container>
                     <Row>
-                        <Col sm={3}>
-                            <img src={`${process.env.PUBLIC_URL}/${p.logo.filename}`}
-                                alt={`Here should be ${p.name} logo`}
-                                title={`${p.name} logo`}
-                                className="img-fluid" />
+                        <Col>
+                            <h3>{p.title}</h3>
                         </Col>
-                        <Col sm={9}>
+                    </Row>
+                    <Row className="mt-3">
+                        <Col sm={6} className="mb-3">
+                            {/* Conditional rendering demo or logo, whatever is available */}
+                            {p.demoFile
+                                ? <img src={`${process.env.PUBLIC_URL}/${p.demoFile}`}
+                                    alt={`Here should be ${p.name} demo`}
+                                    title={`${p.name} demo`}
+                                    className="img-fluid" />
+                                : <img src={`${process.env.PUBLIC_URL}/${p.logo.filename}`}
+                                    alt={`Here should be ${p.name} logo`}
+                                    title={`${p.name} logo`}
+                                    className="img-fluid" />
+                                }
+                        </Col>
+                        <Col sm={6}>
                             <article>
-                                {p.title
+                                {/* {p.title
                                     ? <section>
-                                        <h3>Project name</h3>
+                                        <h4>Project name</h4>
                                         <p>{p.title}</p>
                                     </section>
-                                    : null }
+                                    : null } */}
                                 {p.technologies
                                     ? <section>
-                                        <h3>Technologies</h3>
+                                        <h4>Technologies</h4>
                                         <p>{p.technologies}</p>
                                     </section>
                                     : null }
                                 {p.shortDescription
                                     ? <section>
-                                        <h3>Short description</h3>
+                                        <h4>Short description</h4>
                                         <p>{p.shortDescription}</p>
                                     </section>
                                     : null }
                                 {p.fullDescription
                                     ? <section>
-                                        <h3>Full description</h3>
+                                        <h4>Full description</h4>
                                         <p>{p.fullDescription}</p>
                                     </section>
                                     : null }
                                 {p.contribution.length
                                     ? <section>
-                                        <h3>What I did</h3>
+                                        <h4>What I did</h4>
                                         <ul>
                                             {p.contribution.map(c => <li>c</li>)}
                                         </ul>
@@ -54,21 +66,21 @@ const IndividualProjectMain = (props) => {
                                     : null }
                                 {p.liveLink
                                     ? <section>
-                                        <h3>{p.name} live</h3>
+                                        <h4>{p.name} live</h4>
                                     <p>
                                         Check the actual {p.name} web page <a href={p.liveLink} title={`${p.name} webpage`}>here</a>
                                     </p>
                                     </section>
                                     : null }
-                                {p.demoFile
+                                {/* {p.demoFile
                                     ? <section>
-                                        <h3>{p.name} demo</h3>
+                                        <h4>{p.name} demo</h4>
                                         <img src={`${process.env.PUBLIC_URL}/${p.demoFile}`}
                                             alt={`Here should be ${p.name} demo`}
                                             title={`${p.name} demo`}
                                             className="img-fluid" />
                                     </section>
-                                    : null }
+                                    : null } */}
                             </article>
                         </Col>
                     </Row>
