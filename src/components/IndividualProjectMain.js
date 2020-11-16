9b6a410bca0a2cf24e4812b42f9076a90ecc914a
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2020-11-16 14:57:18 +0300

    Add BS Carousel with individual project details (test mode)

--- src/components/IndividualProjectMain.js
+++ src/components/IndividualProjectMain.js
@@ -96,6 +96,163 @@ const IndividualProjectMain = (props) => {
                         </Col>
                     </Row>
                 </Container>
+
+                    <Carousel indicators={false}>
+                        <Carousel.Item>
+                            <Container style={{width: "70%"}}> 
+                            <Row>
+                                    <Col>
+                                        <h3>{p.title}</h3>
+                                    </Col>
+                                </Row>
+                                <Row className="mt-3">
+                                    <Col sm={6} className="mb-3">
+                                        {/* Conditional rendering demo or logo, whatever is available */}
+                                        {p.demoFile
+                                            ? <img src={`${process.env.PUBLIC_URL}/${p.demoFile}`}
+                                                alt={`Here should be ${p.name} demo`}
+                                                title={`${p.name} demo`}
+                                                className="img-fluid" />
+                                            : <img src={`${process.env.PUBLIC_URL}/${p.logo.filename}`}
+                                                alt={`Here should be ${p.name} logo`}
+                                                title={`${p.name} logo`}
+                                                className="img-fluid" />
+                                            }
+                                    </Col>
+                                    <Col sm={6}>
+                                        <article>
+                                            {/* {p.title
+                                                ? <section>
+                                                    <h4>Project name</h4>
+                                                    <p>{p.title}</p>
+                                                </section>
+                                                : null } */}
+                                            {p.technologies
+                                                ? <section>
+                                                    <h4>Technologies</h4>
+                                                    <p>{p.technologies}</p>
+                                                </section>
+                                                : null }
+                                            {p.shortDescription
+                                                ? <section>
+                                                    <h4>Short description</h4>
+                                                    <p>{p.shortDescription}</p>
+                                                </section>
+                                                : null }
+                                            {p.fullDescription
+                                                ? <section>
+                                                    <h4>Full description</h4>
+                                                    <p>{p.fullDescription}</p>
+                                                </section>
+                                                : null }
+                                            {p.contribution.length
+                                                ? <section>
+                                                    <h4>What I did</h4>
+                                                    <ul>
+                                                        {p.contribution.map(c => <li>c</li>)}
+                                                    </ul>
+                                                </section>
+                                                : null }
+                                            {p.liveLink
+                                                ? <section>
+                                                    <h4>{p.name} live</h4>
+                                                <p>
+                                                    Check the actual {p.name} web page <a href={p.liveLink} title={`${p.name} webpage`}>here</a>
+                                                </p>
+                                                </section>
+                                                : null }
+                                            {/* {p.demoFile
+                                                ? <section>
+                                                    <h4>{p.name} demo</h4>
+                                                    <img src={`${process.env.PUBLIC_URL}/${p.demoFile}`}
+                                                        alt={`Here should be ${p.name} demo`}
+                                                        title={`${p.name} demo`}
+                                                        className="img-fluid" />
+                                                </section>
+                                                : null } */}
+                                        </article>
+                                    </Col>
+                                </Row>
+                                </Container>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                        <Container style={{width: "70%"}}> 
+                                <Row>
+                                    <Col>
+                                        <h3>2{p.title}</h3>
+                                    </Col>
+                                </Row>
+                                <Row className="mt-3">
+                                    <Col sm={6} className="mb-3">
+                                        {/* Conditional rendering demo or logo, whatever is available */}
+                                        {p.demoFile
+                                            ? <img src={`${process.env.PUBLIC_URL}/${p.demoFile}`}
+                                                alt={`Here should be ${p.name} demo`}
+                                                title={`${p.name} demo`}
+                                                className="img-fluid" />
+                                            : <img src={`${process.env.PUBLIC_URL}/${p.logo.filename}`}
+                                                alt={`Here should be ${p.name} logo`}
+                                                title={`${p.name} logo`}
+                                                className="img-fluid" />
+                                            }
+                                    </Col>
+                                    <Col sm={6}>
+                                        <article>
+                                            {/* {p.title
+                                                ? <section>
+                                                    <h4>Project name</h4>
+                                                    <p>{p.title}</p>
+                                                </section>
+                                                : null } */}
+                                            {p.technologies
+                                                ? <section>
+                                                    <h4>Technologies</h4>
+                                                    <p>{p.technologies}</p>
+                                                </section>
+                                                : null }
+                                            {p.shortDescription
+                                                ? <section>
+                                                    <h4>Short description</h4>
+                                                    <p>{p.shortDescription}</p>
+                                                </section>
+                                                : null }
+                                            {p.fullDescription
+                                                ? <section>
+                                                    <h4>Full description</h4>
+                                                    <p>{p.fullDescription}</p>
+                                                </section>
+                                                : null }
+                                            {p.contribution.length
+                                                ? <section>
+                                                    <h4>What I did</h4>
+                                                    <ul>
+                                                        {p.contribution.map(c => <li>c</li>)}
+                                                    </ul>
+                                                </section>
+                                                : null }
+                                            {p.liveLink
+                                                ? <section>
+                                                    <h4>{p.name} live</h4>
+                                                <p>
+                                                    Check the actual {p.name} web page <a href={p.liveLink} title={`${p.name} webpage`}>here</a>
+                                                </p>
+                                                </section>
+                                                : null }
+                                            {/* {p.demoFile
+                                                ? <section>
+                                                    <h4>{p.name} demo</h4>
+                                                    <img src={`${process.env.PUBLIC_URL}/${p.demoFile}`}
+                                                        alt={`Here should be ${p.name} demo`}
+                                                        title={`${p.name} demo`}
+                                                        className="img-fluid" />
+                                                </section>
+                                                : null } */}
+                                        </article>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </Carousel.Item>
+                    </Carousel>
             </main>               
         )
     }
